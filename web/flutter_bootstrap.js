5db352368cb4718916cfa3321031d9ef3b5bf476
mobile-app-4563 <mobileapp.dev.mohamedfasil@gmail.com>
2026-08-13 15:21:51 +0530

    initial commit for rebacca version

--- web/flutter_bootstrap.js
+++
@@ -1,9 +0,0 @@
-{{flutter_js}}
-{{flutter_build_config}}
-
-_flutter.loader.load({
-  config: {
-    // Force local CanvasKit so web works without internet access.
-    canvasKitBaseUrl: "canvaskit/",
-  },
-});
